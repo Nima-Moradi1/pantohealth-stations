b26696d7a8a593bd3dcc2083af5b3c0a4eed8b45
n_moradirad <n_moradirad@hesabo.com>
2026-01-05 09:08:50 +0330

    fix : removed the duplicate App component + updated the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import 'leaflet/dist/leaflet.css';
 import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
-import App from './App.tsx'
+import {App} from './app/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
